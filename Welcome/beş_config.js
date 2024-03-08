01b2118cf3a4131f3e99aca3c591e85b57a53a51
westinaa <162748407+westinaa@users.noreply.github.com>
2024-03-09 00:08:41 +0300

    Create beş_config.js

---
+++ Welcome/beş_config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presence: "west", // Bot Durumu
+  guildID: "1215049428373606440", // Sunucu ID'si
+  mp3: true, // mp3 mü Yoksa Youtube Üzerindenmi Çalmasını İsterseniz Burdan true veya false Yazın.
+  file: "./ses.mp3", // mp3'ü Seçtiyseniz Ses Dosyasının Uzantısı
+  staffRole: "1215662440948441128",//En Alt Yetkili Rol ID'si
+  youtubeURL: "https://www.youtube.com/watch?v=pQ_T0EiWccM", // Youtube'u Seçtiyseniz Bir Youtube URL'si
+  tokens: [ // Tokenler ["55555","5555"] Şeklinde Giriniz.
+    "MTIxNTY3NzcxODc3NjEyMzQ2NA.GjKUv_.ID5CcdhZuQD-guHhm9iwxsFr8Sri0YMm1F_3PA",
+    
+  ],
+  channels: [ // Welcome Kanallarının ID'leri ["55555","5555"] Şeklinde Giriniz.
+    "1215049428373606444", "1215053088113557525"
+    
+  ]
+}
